fca91f426dd301c7a8fb444690547f5da1ba0305
gonzalo <gonzalosuarez@frba.utn.edu.ar>
2024-04-25 18:49:24 -0300

    subo contacto

--- php/contact.php
+++ php/contact.php
@@ -57,7 +57,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "example@themeforest.net";
+$address = "gonzalosuarez997@gmail.com";
 
 
 // Configuration option.
